56ba6b644dc04c7266d9db02b0a48117ae0a2f05
Verejan Cristian <verejancr@gmail.com>
2021-04-06 23:22:11 +0300

    add objects file

---
+++ 010_objects.php
@@ -0,0 +1,20 @@
+<?php
+
+class Car {
+    //constructor
+    public function __construct($brand, $year)
+    {
+        $this->brand = $brand;
+        $this->year = $year;
+    }
+
+    public function print_details(){
+        echo "This car is a " . $this->year . " " . $this->brand . ".\n";
+    }
+}
+
+$car = new Car("Toyota", 2006);
+$car->print_details();
+
+
+?>
