e19c0d4b2721e1347c483577ce19d5bc72ee5341
Verejan Cristian <verejancr@gmail.com>
2021-04-06 21:59:42 +0300

    add variables and simple arrays modified

--- 003_simple_arrays.php
+++ 003_simple_arrays.php
@@ -1,1 +1,15 @@
 <?php
+
+// TODO: add the even_numbers array here
+
+$male_names = ["Jake", "Eric", "John"];
+$female_names = ["Jessica", "Beth", "Sandra"];
+$even_numbers = [2, 4, 6, 8, 10];
+
+// TODO: join the male and female names to one array
+$names = array_merge($male_names, $female_names);
+
+print_r($even_numbers);
+print_r($names);
+
+?>
